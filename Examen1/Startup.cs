e37033645df38df8a8ed0da8d085fc705724790c
Felipeu548 <felipeu548@gmail.com>
2018-12-05 11:11:53 -0300

    Agregar archivos de proyecto.

---
+++ Examen1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Examen1.Startup))]
+namespace Examen1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
